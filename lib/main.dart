5dd771ea141e8a498a413cf2dfa85454537d766b
mart-gant <martgant@gmail.com>
2025-06-18 12:59:08 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/dashboard_screen.dart';
 
-void main() => runApp(const BankApp());
+void main() {
+  runApp(const BankApp());
+}
 
 class BankApp extends StatelessWidget {
   const BankApp({super.key});
@@ -11,10 +15,15 @@ class BankApp extends StatelessWidget {
     return MaterialApp(
       title: 'BankApp',
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
-        colorSchemeSeed: Colors.indigo,
       ),
-      home: const LoginScreen(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (_) => const LoginScreen(),
+        '/register': (_) => const RegisterScreen(),
+        '/dashboard': (_) => const DashboardScreen(),
+      },
     );
   }
 }
