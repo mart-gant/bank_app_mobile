b08a2e950f01ac6e5a88064f72cd9bb04ec6594a
mart-gant <martgant@gmail.com>
2025-06-21 12:30:39 +0200

    Create app_theme.dart

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+  useMaterial3: true,
+);
+
+final ThemeData darkTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: Brightness.dark,
+  ),
+  useMaterial3: true,
+);
